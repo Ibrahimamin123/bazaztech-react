3af548b7246402818d406e6787ab733f23628991
Ibrahimamin123 <ibrahimamin01032004@gmail.com>
2026-06-27 12:22:10 +0500

    Fix Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,21 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
-import CaseStudies from "./components/CaseStudies";
+import Casestudies from "./components/Casestudies";
 import CorporateTraining from "./components/CorporateTraining";
-
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-
-
-       <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/casestudies" element={<CaseStudies />} />
+        <Route path="/casestudies" element={<Casestudies />} />
         <Route path="/corporatetraining" element={<CorporateTraining />} />
       </Routes>
-           
     </BrowserRouter>
   );
 }
